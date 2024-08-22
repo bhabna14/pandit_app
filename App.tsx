90b0d4bdba1df3836307df02444aee747d69b219
sidhanta33crs <sidhanta.rout@33crores.com>
2024-08-22 18:57:40 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -38,8 +38,8 @@ import PrivacyPolicy from './src/Screens/PrivacyPolicy/Index'
 
 const Stack = createNativeStackNavigator()
 
-export const base_url = "https://panditapp.mandirparikrama.com/"
-// export const base_url = "https://pandit.33crores.com/"
+// export const base_url = "https://panditapp.mandirparikrama.com/"
+export const base_url = "https://pandit.33crores.com/"
 
 const App = () => {
 
